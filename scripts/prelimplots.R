468139f0392fe3bcc0e2af35bcb990c586e83d31
sjc6663 <82610745+sjc6663@users.noreply.github.com>
2023-03-22 09:42:42 -0400

    make some preliminary plots

--- scripts/prelimplots.R
+++ scripts/prelimplots.R
@@ -1,6 +1,7 @@
 ## Exploratory Plots
 ## SAB - last updated 2/15/2023
 
+## ---- setup ----
 # load packages
 library(BiocManager)
 library(dplyr)
@@ -15,6 +16,7 @@ load("data/wrangling.RData")
 # load metadata
 meta <- read.delim("/Users/stephanieclouser/Library/CloudStorage/OneDrive-ThePennsylvaniaStateUniversity/Shared-Projects/rhAMR_preliminary/rhAMR_miseq-analysis/sampleinfo/rhamr-samples.txt", sep = "\t")
 
+## ---- merge data ----
 
 # merge metadata with melted df
 # first we need to correct the column names in meltdf
@@ -30,6 +32,26 @@ genefilt <- genes[2:5] %>%
 # merge genes table with other data
 meltdf2 <- merge(meltdf2, genefilt, by = "Gene")
 
+
+## ---- OPTIONS FOR EXPLORATORY PLOTS ----
+
+# make a heatmap
+ggplot(meltdf2 %>% 
+         filter(counts != "0"), aes(x = Gene, fill = Class, y = rh_ID)) +
+  geom_tile() +
+  theme_bw() +
+  coord_fixed() + 
+  theme(legend.position = "none") +
+  theme(axis.text.x = element_text(angle = 90, hjust = 1, size = 4))
+
+# make a barplot
+ggplot(meltdf2 %>% 
+         filter(counts != "0") %>% 
+         filter(rh_ID == "rh02"), aes(x = rh_ID, fill = Class, y = counts)) +
+  geom_bar(stat = "identity") + 
+  scale_y_log10() +
+  facet_wrap(~Broadclass) + # can facet wrap by male vs female
+  theme(legend.position = "none")
 
 
 
