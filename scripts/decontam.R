28937135e9829b80e8daa8a1ed17254c5336e8b4
sjc6663 <82610745+sjc6663@users.noreply.github.com>
2023-03-31 14:09:56 -0400

    update to remove unwanted samples

--- scripts/decontam.R
+++ scripts/decontam.R
@@ -28,6 +28,14 @@ ps <- ps %>%
     SampleBinary = if_else(str_detect(Sample.Type,"Control"), true = "Control", false = "Sample")
   ) 
 
+ps <- ps %>% 
+  ps_mutate(SampleBinary = case_when(
+  SampleBinary == "Control" & str_detect(Sample.Type, "Pos") ~ "Sample",
+  SampleBinary == "Control" & !str_detect(Sample.Type, "Pos") ~ "Control",
+  SampleBinary == "Sample" ~ "Sample"
+))
+
+
 # inspect # of counts per sample
 sam <- as.data.frame(sample_data(ps))
 sam$GeneCounts <- sample_sums(ps)
@@ -39,9 +47,9 @@ sam$Index <- seq(nrow(sam))
 ggplot(sam, aes(x = Index, y = GeneCounts, color = SampleBinary)) +
   geom_point()
 
-
+## Negative Control ----
 sample_data(ps)$is.neg <- sample_data(ps)$SampleBinary == "Control"
-contamdf.prev <- isContaminant(ps, method = "prevalence", neg = "is.neg")
+contamdf.prev <- isContaminant(ps, method = "prevalence", neg = "is.neg", threshold = 0.5)
 
 # make a phyloseq object of only the contaminants and controls
 con <- rownames(contamdf.prev[contamdf.prev$contaminant == "TRUE",])
@@ -55,20 +63,56 @@ contams
 # remove contaminant sequences
 nocontam <- prune_taxa(!rownames(ps@tax_table) %in% contams, ps)
 
-# get a name of the controls list
-controls <- ps %>%
-  ps_filter(SampleBinary == "Control", .keep_all_taxa = TRUE)#or whatever you named that column
+saveRDS(nocontam, "data/ransom/decontam-ps.rds")
 
-controls <- sample_data(controls)$Sample.ID #or whatver you have your sample ID column named
+## Positive Control ----
 
-# remove all controls
-nocontrol <- subset_samples(
-  nocontam,
-  SampleBinary == "Sample"
+zymo <- readRDS("/Users/stephanieclouser/OneDrive - The Pennsylvania State University/Shared-Projects/1.Tutorials/ZymoAMR/zymo-amr.rds")
+
+# get positive controls after removing contaminants
+pspos <- nocontam %>% 
+  ps_filter(str_detect(Sample.Type, "Pos")) 
+
+ntaxa(pspos)
+
+ntaxa(zymo)
+
+get_taxa_unique(pspos)
+get_taxa_unique(zymo)
+
+# we don't want to remove the metals and biocides since we can't compare them to the zymo so we filter out only the drugs and multi-compounds
+pstruepos <- subset_taxa(pspos, Broadclass == "Drugs")
+get_taxa_unique(pstruepos)
+
+# get what we want to remove
+psposrm <- subset_taxa(pstruepos, !taxa_names(pstruepos) %in% taxa_names(zymo))
+
+# we need to remove the negative control from the sample set
+pssave <- pssave <- ps_filter(nocontam, SampleBinary == "Sample")
+
+# giving us the object decontaminated with all samples
+pscount <- subset_taxa(ps, taxa_names(ps) %in% taxa_names(pssave))
+
+# remove positive controls
+ps.only.sample <- prune_samples(sample_data(pscount)$Sample.Type != "Positive-Control", pscount)
+ps.only.sample2 <- prune_samples(sample_data(ps.only.sample)$Sample.Type != "Negative-Control", ps.only.sample)
+
+
+# remove contaminant sequences
+fin_nocontam <- subset_taxa(ps.only.sample2, !taxa_names(pscount) %in% taxa_names(psposrm))
+
+# removing SNP confirmation genes ----
+psfilt <- fin_nocontam %>% tax_select(tax_list = "SNP", strict_matches = FALSE, deselect = TRUE)
+
+Keep <- c("Cows", "Calves")
+
+psfilt <- subset_samples(
+  psfilt, 
+  Group %in% Keep
 )
 
 # save the decontaminated phyloseq object for downstrem analysis
-saveRDS(nocontrol, file = "data/ransom/decontam-ps.rds")
+saveRDS(psfilt, file = "data/ransom/decontam-ps.rds")
 
 # save work
 save.image("data/decontam.RData")
