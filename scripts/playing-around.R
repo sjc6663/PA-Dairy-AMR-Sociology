4fa74611e67327f18a08154bea0a2f3f2692427c
sjc6663 <82610745+sjc6663@users.noreply.github.com>
2023-04-04 11:49:54 -0400

    playing around with visuals and data frames

--- scripts/playing-around.R
+++ scripts/playing-around.R
@@ -1,3 +1,21 @@
+
+ps <- readRDS("data/ransom/decontam-ps.RDS")
+
+ps <- ps0
+
+ps0@sam_data <- sample_data(samp)
+
+group <- c("Cows", "Calves")
+
+ps0 <- subset_samples(
+  ps0,
+  Group %in% group
+)
+
+# save the decontaminated phyloseq object for downstrem analysis
+saveRDS(ps0, file = "data/ransom/decontam-ps.rds")
+
+
 ## ---- create data frames for each breed and type ----
 adiv <- data.frame(
   # "Observed" = phyloseq::estimate_richness(H_fecal_counts, measures = "Observed"),
@@ -10,13 +28,13 @@ head(adiv)
 test <- aov(Shannon ~ Conventional.Organic, data = adiv)
 summary(test) # p = 0.0247*
 
-plot_richness(ps, x="Male.Female", measures=c("Shannon"), title = "A", color = "Towel.Type") + 
+plot_richness(ps, x="Male.Female", measures=c("Shannon"), title = "Males vs Females", color = "Number.Milking.Cows") + 
   geom_boxplot() + 
   scale_color_manual(values = c("#a76119", "#028571", "#dfc27d", "#80cdc1", "#999999")) +
-  theme_classic()
-  # theme(legend.position = "none")
+  theme_classic() +
+  theme(legend.position = "none")
 A
-ggsave(plot = A, filename = "plots/alpha-diversity-holstein-fecal.pdf", dpi = 600)
+ggsave(filename = "plots/alpha-diversity-mf.pdf", dpi = 600)
 
 
 # transform to relative abundance
@@ -45,3 +63,71 @@ psrel %>%
   ggtitle("A") +
   #theme(legend.position = "none") +
   labs(caption = "")
+
+
+## BAR PLOTS ----
+
+male <- subset_samples(
+  ps,
+  Male.Female == "Male"
+)
+
+female <- subset_samples(
+  ps,
+  Male.Female == "Female"
+)
+
+df.male.otu <- as.data.frame(male@otu_table)
+df.male.tax <- as.data.frame(male@tax_table)
+
+df.male.otu <- rownames_to_column(df.male.otu, var = "Gene")
+
+df.male <- merge.data.frame(df.male.otu, df.male.tax)
+ 
+bmale <- ggplot(df.male, aes(x=Class, fill = Broadclass)) + 
+  geom_bar()+
+  theme_bw()+
+  ggtitle("Males") + 
+  scale_fill_viridis(discrete=TRUE, option = "A", begin = 0.1, end = 0.9) +
+  theme(legend.position = "none") +
+  theme(axis.text.x = element_text(angle = 90, hjust = 1))
+bmale
+
+df.female.otu <- as.data.frame(female@otu_table)
+df.female.tax <- as.data.frame(female@tax_table)
+
+df.female.otu <- rownames_to_column(df.female.otu, var = "Gene")
+
+df.female <- merge.data.frame(df.female.otu, df.female.tax)
+
+bfemale <- ggplot(df.female, aes(x=Broadclass, fill = Class)) + 
+  geom_bar()+
+  theme_bw()+
+  ggtitle("Females") + 
+  scale_fill_viridis(discrete=TRUE, option = "A", begin = 0.1, end = 0.9) +
+  theme(legend.position = "none")
+bfemale
+
+library(patchwork)
+bmale/bfemale
+
+df.otu <- as.data.frame(ps0@otu_table)
+df.tax <- as.data.frame(ps0@tax_table)
+df.otu <- rownames_to_column(df.otu, var = "Gene")
+df <- merge.data.frame(df.otu, df.tax)
+df2 <- t(df)
+df2 <- as.data.frame(df2)
+
+df.met <- as.data.frame(ps0@sam_data)
+df2 <- rownames_to_column(df2, var = "Sample.ID")
+df3 <- merge.data.frame(met, df2)
+
+p <- ggplot(df, aes(x=Broadclass, fill = Class)) + 
+  geom_bar()+
+  theme_bw()+
+  ggtitle("Males v. Females") + 
+  scale_fill_viridis(discrete=TRUE, option = "A", begin = 0.1, end = 0.9) +
+  theme(legend.position = "none")
+
+
+p
