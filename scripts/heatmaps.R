230726e518fe0a151e0101ee81d9ab337199839e
sjc6663 <82610745+sjc6663@users.noreply.github.com>
2023-04-19 09:35:56 -0400

    heatmaps wrangling

--- scripts/heatmaps.R
+++ scripts/heatmaps.R
@@ -1,191 +1,151 @@
 ## TRYING TO DO HEATMAPS
 ## 4/4/23 SAB
 
+### GOOD ##############################################################################################################################
+# https://towardsdatascience.com/pheatmap-draws-pretty-heatmaps-483dab9a3cc
 
-plot_heatmap(ps, sample.label = "Group", taxa.label = "Broadclass")
-
-heatmap(ps, scale = "column")
-
-sample_data(psrel)
-df.matrix <- data.matrix(meltdf)
-
-heatmap(df.matrix, Rowv = NA,
-        Colv = NA, col = color_palette, scale = "column")
-
-
+## WRANGLING GENE METADATA FOR HEATMAP ----
+# create data frame from phyloseq object
 meltdf2 <- psmelt(psrel)
 
-new <- meltdf %>% select(c("OTU", "Sample", "Abundance"))
-new.mat <- data.matrix(meltdf3)
+# remove any genes that have 0 relative abundance for samples
+meltdf3 <- meltdf2[meltdf2$Abundance != 0.000000000000, ]
 
+# from the data table, select only the columns we want for gene info and abundance
+genemeta <- dplyr::select(meltdf3, Sample.ID, Broadclass, Class, Abundance) 
 
-ggplot(meltdf, aes(x = Sample, y = OTU, fill = Abundance)) +
-  geom_tile() +
-  scale_fill_distiller(palette = "Spectral") +
-  theme(axis.text.x = element_text(angle = 90, hjust = 1))
+# convert to data frame
+genemeta <- as.data.frame(genemeta)
 
+# combine all columns that are the same resistance type for each sample
+genemeta <- setDT(genemeta)[, .(relabund = sum(Abundance)), by = list(Sample.ID, Broadclass, Class)]
 
+# select only the gene metadata tables we want (Broadclass and Class)
+genemeta <- dplyr::select(genemeta, Broadclass, Class)
 
-# BASE R PACKAGE
+# combine all groups that are the same
+test <- Aggregate(genemeta, c("Class", "Broadclass"))
+genemeta <- dplyr::select(test, Broadclass, Class)
 
-heatmap(out2,
-        xlab = "Sample",
-        ylab = "Abundance")
-otu_table(ps)
+genemeta$Broadclass <- as.factor(genemeta$Broadclass) # change broadclass to a factor
+genemeta <- as.data.frame(genemeta) # make into a data frame so it can be called for annotation_row =
+rownames(genemeta) <- genemeta$Class
+genemeta2 <- genemeta[order(genemeta$Broadclass, genemeta$Class),] # sort this so rows will cluster by broadclass and not alphabetical
 
-out <- as.data.frame(otu_table(ps))
-out2 <- data.matrix(out)
-
-
-
-
-all_site_v4
-
-#filter out rows with 0 relabun 
-meltdf3 <- meltdf2[meltdf2$Abundance != 0.0000000000, ]
-
-#set color vars
-colors <- list(
-  "Male.Female" = c(Male = "#000004FF", Female = "#24868EFF"),
-  "Group" = c(Calves = "#7DCCFF", Cows = "#098BD9"),
-  "Broadclass" = c("Biocides" = "#552F7A",
-                        "Drugs" = "#7C5F98",
-                        "Metals" = "#B09FC1",
-                        "Multi-compound" = "#CABED6"))
-
-
-pheatmap(new.mat, 
-           annotation_col = subset(x=meltdf3, select = c("Male.Female", "Group")),
-           annotation_row = subset(x=meltdf3, select = "Broadclass"),
-           #cluster_cols = hclust(dist(t(out), method = "euclidean")), 
-           cluster_rows = F,
-           annotation_colors = colors,
-           color = inferno(15),
-           gaps_row = c(8,31,43),
-           fontsize = 15,
-           annotation_legend = F,
-           legend = T,
-           show_rownames = TRUE,
-           border_color = "black",
-          show_colnames = FALSE)
-
-
-
-
-
-
-
-
-
-
-##############################################################################################################################
-
-# read in phyloseq object
-ps <- readRDS("data/full-run/decontam-ps.RDS")
-
-meltdf <- psmelt(ps)
-
-#filter out rows with 0 relabun 
-out2 <- meltdf[meltdf$Abundance != 0.0000000000, ]
-
-
-#first row annotations
-genemeta <- out2 %>% dplyr::select(Sample, Broadclass, Class, Abundance) %>% 
-  group_by(Sample, Class)%>%
-  pivot_wider(values_from = Abundance) %>% 
-  dplyr::select(Broadclass, Class)
-
-genemeta %>% pivot_wider(values_from = Abundance)
-
-
-  genemeta$Broadclass <- as.factor(genemeta$Broadclass) #change broadclass to factor
-  genemeta <- as.data.frame(genemeta) #make into df so it can be called for annotation_row =
-  rownames(genemeta) <- genemeta$Class #convert rownames to match o6
-  genemeta2 <- genemeta[order(genemeta$Broadclass, genemeta$Class),] #sort this so rows will cluster by broadclass and not just be plotted alphabetically
-
-  
 #fix row names for plot
 gm2names <- rownames(genemeta2)
 gm2names <- str_replace_all(gm2names, "_", " ") #sub underscores with spaces
 gm2names <- str_replace(gm2names, "resistance", "")#drop resistance because that's a given
 gm2names <- str_replace_all(gm2names, "and", "+") #simplfy this to make shorter
 
-rownames(genemeta2) <- gm2names #assign back
-#fix colnames for aesthetic purposes
-colnames(genemeta2)[1] <- "Sample"
-colnames(genemeta2)[3] <- "Resistance Type"    
-genemeta2
+rownames(genemeta2) <- gm2names
+
+colnames(genemeta2)[1] <- "Resistance Type"
+
+write.table(genemeta2, file = "tables/genemetadata_cleaned.txt", sep = "\t")
+
+## WRANGLING SAMPLE METADATA FOR HEATMAP ----
 
 metfilt <- met %>% 
   filter(`Sample-Type` == "Sample")
 
-metfilt
-
-#fix rownames like with genemeta2
-rownames(out2) <- str_replace_all(rownames(out2), "_", " ")#sub underscores with spaces
-rownames(o6) <- str_replace(rownames(o6), "resistance", "")#drop resistance because that's a given
-rownames(o6) <- str_replace_all(rownames(o6), "and", "+") #simplfy this to make shorter
-
-
-#set color vars
-colors <- list(
-  "Male.Female" = c(Male = "#000004FF", Female = "#24868EFF"),
-  "Group" = c(Calves = "#7DCCFF", Cows = "#098BD9"),
-  "Broadclass" = c("Biocides" = "#552F7A",
-                   "Drugs" = "#7C5F98",
-                   "Metals" = "#B09FC1",
-                   "Multi-compound" = "#CABED6"))
+# edit metadata so that it is numeric so that it can be used in the heatmap
+is.character(metfilt$`Male/Female`)
+metfilt$`Male/Female`[metfilt$`Male/Female` == "Male"] <- 1
+metfilt$`Male/Female`[metfilt$`Male/Female` == "Female"] <- 2
+is.numeric(metfilt$`Male/Female`)
+metfilt$`Male/Female` <- as.numeric(metfilt$`Male/Female`)
+is.numeric(metfilt$`Male/Female`)
 
 
-mat <- out2[match(gm2names, rownames(out2)),]
-
-  pheatmap(meltdf, 
-           annotation_col = subset(x=meltdf, select = c("Male.Female", "Herd.Size")),
-           annotation_row = subset(x=meltdf, select = "Class"),
-           #cluster_cols = hclust(dist(t(out2), method = "euclidean")), 
-          # cluster_rows = F,
-           annotation_colors = colors,
-           color = inferno(15),
-           gaps_row = c(8,31,43),
-           fontsize = 15,
-           annotation_legend = F,
-           legend = T,
-           show_rownames = TRUE,
-           border_color = NA)
+is.character(metfilt$`Herd-Size`)
+metfilt$`Herd-Size`[metfilt$`Herd-Size` == "0-50"] <- 0
+metfilt$`Herd-Size`[metfilt$`Herd-Size` == "50-100"] <- 1
+metfilt$`Herd-Size`[metfilt$`Herd-Size` == "100-150"] <- 2
+metfilt$`Herd-Size`[metfilt$`Herd-Size` == "150-200"] <- 3
+metfilt$`Herd-Size`[metfilt$`Herd-Size` == "200+"] <- 4
+is.numeric(metfilt$`Herd-Size`)
+metfilt$`Herd-Size` <- as.numeric(metfilt$`Herd-Size`)
+is.numeric(metfilt$`Herd-Size`)
 
 
-df_num <- as.matrix(genemeta2[4])
+# column names for aesthetics
+colnames(metfilt)[6] <- "Male v. Female Operator"
+colnames(metfilt)[54] <- "Milking Herd Size"
 
-### GOOD ##############################################################################################################################
+rownames(metfilt) <- metfilt$`Sample-ID`
 
+# make into dataframe so it can be called in annotation_col = 
+dfmetfilt <- as.matrix(metfilt)
+
+
+## COUNTS FOR HEATMAP FILL ----
 # get counts and genes
-counts <- as.data.frame(ps@otu_table)
-gene <- as.data.frame(ps@tax_table)
+counts <- as.data.frame(psrel@otu_table)
+gene <- as.data.frame(psrel@tax_table)
+
+gene2 <- dplyr::select(gene, Broadclass, Class)
 
 counts2 <- rownames_to_column(counts, var = "Gene")
 
+# combine counts and gene info
 merge <- merge(counts2, gene)
 write.table(merge, file = "tables/merged-counts-and-gene-clean.txt", sep = "\t")
 
+# select only the columns we want (samples and class)
 merge2 <- merge %>% dplyr::select(R23:Class)
 merge3 <- merge2 %>% dplyr::select(!Broadclass)
 
 # collapse the classes so that they are only one row
 out3 <- aggregate(.~Class, data = merge3, FUN = sum)
+write.table(out3, file = "tables/clr-relabund_byclass.txt", sep = "\t")
 
 counts2 <- column_to_rownames(out3, "Class")
 write.table(counts2, file = "tables/class-count-data-only.txt", sep = "\t")
 
-df_num_scale <- scale(counts2)
-pheatmap(df_num_scale, 
-         #annotation_col = subset(x = mat, select = c("Male.Female", "Herd.Size")),
-         annotation_row = subset(x = merge, select = "Broadclass"),
+# fix rowname issue with the data
+out3 <- rownames_to_column(out3, "XXX")
+out3 <- column_to_rownames(out3, "Class")
+out3 <- dplyr::select(out3, !XXX)
+
+# Aesthetics
+#fix rownames like with genemeta2
+rownames(out3) <- str_replace_all(rownames(out3), "_", " ")#sub underscores with spaces
+rownames(out3) <- str_replace(rownames(out3), "resistance", "")#drop resistance because that's a given
+rownames(out3) <- str_replace_all(rownames(out3), "and", "+") #simplfy this to make shorter
+
+
+#now that our rownames match lets sort them to match order of gm2names
+mat <- out3[match(gm2names, rownames(out3)),]
+
+#set color vars
+colors <- list(
+  "Male v. Female Operator" = c(Male = "#088F8F", Female = "#5F9EA0"),
+  #"Group" = c(Calves = "#1F51FF", Cows = "#1434A4"),
+  "Milking Herd Size" = c("0-50" = "#BCE1FF", "50-100" = "#7DCCFF", "100-150" = "#56B4E9", "150-200" = "#098BD9", "200+" = "#4292C6"),
+  "Broadclass" = c("Biocides" = "#552F7A",
+                   "Drugs" = "#7C5F98",
+                   "Metals" = "#B09FC1",
+                   "Multi-compound" = "#CABED6"))
+
+mat <- as.matrix(mat)
+
+
+test <- pheatmap(mat, 
+         #annotation_col = subset(x=dfmetfilt, select = c("Male v. Female Operator", "Milking Herd Size")),
+         annotation_row = subset(x=genemeta2, select = "Resistance Type"),
+         cluster_cols = hclust(dist(t(out3), method = "euclidean")),
          cluster_rows = F,
-         color = mako(15),
+         annotation_colors = colors,
+         color = inferno(15),
+         gaps_row = c(10,27,43),
+         fontsize = 15,
          annotation_legend = F,
-        # gaps_row = c(8, 31, 43),
-         main = "pheatmap test")
+         legend = T,
+         show_rownames = TRUE,
+         border_color = NA)
 
-
-
+pdf("test.pdf", width = 18, height = 24)
+test
+dev.off()
 
