2adec5357922f1ba5a7d5603783fd7e5f5655d73
sjc6663 <sjc6663@psu.edu>
2023-09-12 12:09:54 -0400

    generate figures for RSS AMR paper

---
+++ scripts/paper/figure7.R
@@ -0,0 +1,130 @@
+# Figure 7 - clinically significant genes, gender (rel abund, alpha, beta)
+# SAB 09/12/2023
+
+# setup ----
+# load packages
+library(phyloseq)
+library(microViz)
+library(ggplot2)
+library(viridis)
+library(BiocManager)
+library(stringr)
+library(microshades)
+library(microbiome)
+library(patchwork)
+library(ggpubr)
+library(knitr)
+library(dplyr)
+library(car)
+library(misty)
+library(vegan)
+
+set.seed(81299)
+
+ps2 <- readRDS("data/full-run/sig-decontam-ps.RDS")
+
+# filter out taxa that are not clinically significant and remove them
+sig <- subset_taxa(ps2, 
+                   sig == "TRUE")
+
+# barplot (A) ----
+psrel <- microbiome::transform(sig, "compositional")
+
+psbclass <- aggregate_taxa(psrel, level = "Broadclass")
+# fix taxa for aesthetics
+# find and substitute
+taxa_names(psbclass) <- gsub(taxa_names(psbclass), pattern = "_", replacement = " ") 
+
+A <- psbclass %>% plot_composition(average_by = "Male.Female", sample.sort = "Male.Female", x.label = "Male.Female") +
+  # change y axis to be percentages instead of numbers
+  scale_y_continuous(labels = scales::percent) +
+  scale_fill_viridis(option = "mako", discrete = TRUE) + 
+  theme(text = element_text(size = 20)) + 
+  theme(legend.position = "top") +
+  labs(x = " ", fill=' ') +
+  scale_x_discrete(guide = guide_axis(angle = 0)) +
+  ggtitle("A")
+A
+
+# relative abundance plot percentages ----
+# merge samples by comparative categories (metadata) we want
+mps <- merge_samples(psrel, "Male.Female")
+
+# create a dataframe with the aggregated abundances for desired tax rank
+phy <- mps %>% tax_glom(taxrank = "Broadclass") %>% transform_sample_counts(function(x) {x/sum(x)}) %>% psmelt()
+
+# select only relevant columns
+phy <- select(phy, c("Sample", "Broadclass", "Abundance"))
+
+# get abundance as a percent and round to whole numbers
+phy$percent <- phy$Abundance * 100
+phy$percent <- round(phy$percent, digits = 0)
+
+phy <- select(phy, "Sample", "Broadclass", "percent")
+
+phy
+
+# alpha diversity (B) ----
+# create data frame with relevant metadata for comparison
+adiv <- data.frame(
+  "Shannon" = phyloseq::estimate_richness(sig, measures = "Shannon"),
+  "MF" = phyloseq::sample_data(sig)$Male.Female
+)
+
+# test variance
+# Male Female 
+varMF <- var.test(Shannon ~ MF, data = adiv, 
+                  alternative = "two.sided")
+varMF # p = 0.55, not sig
+
+# # male female
+wtestMF <- t.test(Shannon ~ MF, data = adiv, var.equal = FALSE)
+wtestMF # p = 0.0005**, SIGNIFICANT
+
+# violin plot 
+ps.meta <- meta(sig)
+ps.meta$Shannon <- phyloseq::estimate_richness(sig, measures = "Shannon")
+
+ps.meta$'' <- alpha(sig, index = 'shannon')
+
+B <- ggviolin(ps.meta, x = "Male.Female", y = "Shannon$Shannon",
+              add = "boxplot", fill = "Group", palette = c("#367aa1", "#def4e5"), title = "B", ylab = "Shannon's Diversity Index", xlab = " ") +
+  theme(legend.position = "none") +
+  theme(text = element_text(size = 20), axis.title = element_text(size = 20)) +
+  scale_y_continuous(limits = c(1, 7))
+B
+
+# beta diversity (C) ----
+# transform to relative abundance
+psrel <- microbiome::transform(sig, "compositional")
+
+# clr transform
+transps <- psrel %>% 
+  tax_transform(trans = "clr") %>% 
+  ps_get()
+
+# create distance matrix 
+dist_mat <- phyloseq::distance(transps, method = "euclidean")
+
+# test
+vegan::adonis2(dist_mat ~ phyloseq::sample_data(transps)$Male.Female) # R2 = 0.019, F(1,70) = 1.34, P = 0.007**
+
+# plot
+
+C <- psrel %>% 
+  # when no distance matrix or constraints are supplied, PCA is the default/auto ordination method
+  tax_transform(trans = "clr") %>%
+  ord_calc(method = "PCA") %>% 
+  ord_plot(color = "Male.Female", size = 6, axes = c(1,2)) +
+  scale_color_manual(values = c("#367aa1", "#def4e5")) +
+  stat_ellipse(aes(group = Male.Female, color = Male.Female)) + 
+  theme_classic() +
+  labs(color = "Gender") +
+  ggtitle("C") + 
+  labs(caption = "R2 = 0.019, F(1,70) = 1.34, P = 0.007**") +
+  theme(text = element_text(size = 20)) 
+C
+
+(A|B)/C
+
+ggsave(filename = "plots/paper/figure7.pdf", dpi = 600, width = 12, height = 10)
--- scripts/paper/paper-figure1.R
+++ scripts/paper/paper-figure1.R
@@ -1,3 +1,6 @@
+# Figure 2 - Barplot of Relative Abundance (small multiples) Calves/Cows + Org/Conv
+# SAB 09/12/2023
+
 # load packages
 library(phyloseq)
 library(microViz)
@@ -9,6 +12,10 @@ library(microshades)
 library(microbiome)
 library(patchwork)
 library(ggpubr)
+#install.packages("gridExtra")
+library(gridExtra)
+
+set.seed(81299)
 
 # read in phyloseq object
 ps <- readRDS("data/full-run/decontam-ps.RDS")
@@ -37,9 +44,9 @@ A <- calves %>% plot_composition(average_by = "Farm", sample.sort = "Farm", x.la
   scale_y_continuous(labels = scales::percent) +
   scale_fill_viridis(option = "mako", discrete = TRUE) + 
   theme(text = element_text(size = 20)) + 
-  theme(legend.position = "top") +
+  theme(legend.position = "none") +
   labs(x = " ", fill=' ') +
-  scale_x_discrete(guide = guide_axis(angle = 0)) +
+  scale_x_discrete(guide = guide_axis(angle = 90)) +
   ggtitle("A")
 
 B <- cows %>% plot_composition(average_by = "Farm", sample.sort = "Farm", x.label = " ") +
@@ -47,9 +54,9 @@ B <- cows %>% plot_composition(average_by = "Farm", sample.sort = "Farm", x.labe
   scale_y_continuous(labels = scales::percent) +
   scale_fill_viridis(option = "mako", discrete = TRUE) + 
   theme(text = element_text(size = 20)) + 
-  theme(legend.position = "top") +
+  theme(legend.position = "none") +
   labs(x = " ", fill=' ') +
-  scale_x_discrete(guide = guide_axis(angle = 0)) +
+  scale_x_discrete(guide = guide_axis(angle = 90)) +
   ggtitle("B")
 
 C <- calves %>% plot_composition(average_by = "Farm", sample.sort = "Conventional.Organic", x.label = "Farm", group_by = "Conventional.Organic") +
@@ -57,9 +64,9 @@ C <- calves %>% plot_composition(average_by = "Farm", sample.sort = "Conventiona
   scale_y_continuous(labels = scales::percent) +
   scale_fill_viridis(option = "mako", discrete = TRUE) + 
   theme(text = element_text(size = 20)) + 
-  theme(legend.position = "top") +
+  theme(legend.position = "none") +
   labs(x = " ", fill=' ') +
-  scale_x_discrete(guide = guide_axis(angle = 0)) +
+  scale_x_discrete(guide = guide_axis(angle = 90)) +
   ggtitle("C")
 
 D <- cows %>% plot_composition(average_by = "Farm", sample.sort = "Conventional.Organic", x.label = "Farm", group_by = "Conventional.Organic") +
@@ -67,18 +74,47 @@ D <- cows %>% plot_composition(average_by = "Farm", sample.sort = "Conventional.
   scale_y_continuous(labels = scales::percent) +
   scale_fill_viridis(option = "mako", discrete = TRUE) + 
   theme(text = element_text(size = 20)) + 
-  theme(legend.position = "top") +
+  theme(legend.position = "none") +
   labs(x = " ", fill=' ') +
-  scale_x_discrete(guide = guide_axis(angle = 0)) +
+  scale_x_discrete(guide = guide_axis(angle = 90)) +
   ggtitle("D")
 
-(A|B)/(C|D)
+legend <- cows %>% plot_composition(average_by = "Farm", sample.sort = "Conventional.Organic", x.label = "Farm", group_by = "Conventional.Organic") +
+  # change y axis to be percentages instead of numbers
+  scale_y_continuous(labels = scales::percent) +
+  scale_fill_viridis(option = "mako", discrete = TRUE) + 
+  theme(text = element_text(size = 40)) + 
+  theme(legend.position = "top") +
+  labs(x = " ", fill=' ') +
+  scale_x_discrete(guide = guide_axis(angle = 90)) +
+  ggtitle("D")
 
-all <- ggarrange(A, B, C, D,
-                 ncol = 2, nrow = 2, 
-                 common.legend = TRUE, legend = "bottom")
+#(A|B)/(C|D)
 
-all
+#all <- ggarrange(A, B, C, D,
+#                 ncol = 2, nrow = 2, 
+#                 common.legend = TRUE, legend = "bottom")
 
-ggsave(filename = "plots/PCA-all-samples.pdf", dpi = 600, width = 12, height = 16)
+#all <- A + B + C + D + plot_layout(ncol = 2, nrow = 2, 
+#                                   guides = "collect") +
+#  theme(legend.position = "top")
+
+# all
+
+# Create user-defined function, which extracts legends from ggplots
+extract_legend <- function(my_ggp) {
+  step1 <- ggplot_gtable(ggplot_build(my_ggp))
+  step2 <- which(sapply(step1$grobs, function(x) x$name) == "guide-box")
+  step3 <- step1$grobs[[step2]]
+  return(step3)
+}
+
+# Apply user-defined function to extract legend
+shared_legend <- extract_legend(legend)
+
+# Draw plots with shared legend
+all <- grid.arrange(arrangeGrob(A, B, C, D, ncol = 2, nrow = 2),
+             shared_legend, nrow = 2, heights = c(10, 1))
+
+ggsave(all, filename = "plots/paper/figure2.pdf", dpi = 600, width = 24, height = 20)
 
