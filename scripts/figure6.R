5ba9e69d898ec034cf05604ffec281f7530c19f1
Stephanie Bierly <82610745+sjc6663@users.noreply.github.com>
2024-05-22 17:57:50 -0400

    Update figure6.R

--- scripts/figure6.R
+++ scripts/figure6.R
@@ -73,7 +73,7 @@ adiv <- data.frame(
 )
 
 # statistical test
-model <- lm(Shannon ~ run + batch + aff + type, data = adiv)
+model <- lm(Shannon ~ run + batch, data = adiv)
 residuals <- resid(model)
 
 res <- as.data.frame(residuals)
@@ -85,7 +85,7 @@ dat1 <- merge(adiv, res, by = "samp")
 dat1 <- column_to_rownames(dat1, "samp")
 
 model2 <- lm(residuals ~ lang, data = dat1)
-summary(model2) # P = 0.93, ns
+summary(model2) # P = 0.94, ns
 
 # violin plot
 ps.meta <- meta(ps)
@@ -108,17 +108,17 @@ ait <- ps %>%
 
 
 # test beta dispersion
-ait %>% dist_bdisp(variables = "Cultural.Language.Barriers") %>% bdisp_get() # p=0.77
+ait %>% dist_bdisp(variables = "Cultural.Language.Barriers") %>% bdisp_get() # p=0.31
 
 # test with PERMANOVA
 mod1 <- ait %>%
   dist_permanova(
     seed = 81299,
-    variables = "Cultural.Language.Barriers + Run + Batch + affiliation + Conventional.Organic",
+    variables = "Cultural.Language.Barriers + Run + Batch",
     n_perms = 9999
   )
 
-mod1 # R2 = 0.01, F(1, 65) = 0.91, P = 0.61
+mod1 # R2 = 0.04, F(1, 24) = 0.94, P = 0.52
 
 C <- psrel %>% 
   # when no distance matrix or constraints are supplied, PCA is the default/auto ordination method
