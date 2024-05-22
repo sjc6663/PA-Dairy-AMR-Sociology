bf8e4e63b8f179408fdf38ebe115ab658587446f
Stephanie Bierly <82610745+sjc6663@users.noreply.github.com>
2024-05-22 17:16:13 -0400

    Update figure2.R

--- scripts/figure2.R
+++ scripts/figure2.R
@@ -20,6 +20,9 @@ set.seed(81299)
 # read in phyloseq object
 ps <- readRDS("bovine-host-resistome/decontam-ps.RDS")
 
+ps <- subset_samples(ps,
+                     Conventional.Organic == "Conventional")
+
 # transform to relative abundance
 psrel <- microbiome::transform(ps, "compositional")
 
@@ -33,61 +36,61 @@ psbclass <- aggregate_taxa(psrel, level = "Broadclass")
 taxa_names(psbclass) <- gsub(taxa_names(psbclass), pattern = "_", replacement = " ")
 
 # subset phyloseq object by calves/cows
-calves <- subset_samples(psbclass,
-                         Group == "Calves")
-cows <- subset_samples(psbclass, 
-                       Group == "Cows")
+#calves <- subset_samples(psbclass,
+#                         Group == "Calves")
+#cows <- subset_samples(psbclass, 
+#                       Group == "Cows")
 
 
-A <- calves %>% plot_composition(average_by = "Farm", sample.sort = "Farm", x.label = " ") +
+#A <- calves %>% plot_composition(average_by = "Farm", sample.sort = "Farm", x.label = " ") +
+#  # change y axis to be percentages instead of numbers
+#  scale_y_continuous(labels = scales::percent) +
+#  scale_fill_viridis(option = "mako", discrete = TRUE) + 
+#  theme(text = element_text(size = 20)) + 
+#  theme(legend.position = "none") +
+#  labs(x = " ", fill=' ') +
+#  scale_x_discrete(guide = guide_axis(angle = 90)) +
+#  ggtitle("A")
+
+#B <- cows %>% plot_composition(average_by = "Farm", sample.sort = "Farm", x.label = " ") +
+  # change y axis to be percentages instead of numbers
+#  scale_y_continuous(labels = scales::percent) +
+#  scale_fill_viridis(option = "mako", discrete = TRUE) + 
+#  theme(text = element_text(size = 20)) + 
+#  theme(legend.position = "none") +
+#  labs(x = " ", fill=' ') +
+#  scale_x_discrete(guide = guide_axis(angle = 90)) +
+#  ggtitle("B")
+
+A <- psbclass %>% plot_composition(average_by = "Farm", sample.sort = "Group", x.label = "Farm", group_by = "Group") +
   # change y axis to be percentages instead of numbers
   scale_y_continuous(labels = scales::percent) +
   scale_fill_viridis(option = "mako", discrete = TRUE) + 
   theme(text = element_text(size = 20)) + 
-  theme(legend.position = "none") +
+  theme(legend.position = "right") +
   labs(x = " ", fill=' ') +
-  scale_x_discrete(guide = guide_axis(angle = 90)) +
-  ggtitle("A")
+  scale_x_discrete(guide = guide_axis(angle = 90)) 
+ # ggtitle("A")
 
-B <- cows %>% plot_composition(average_by = "Farm", sample.sort = "Farm", x.label = " ") +
+#B <- psbclass %>% plot_composition(average_by = "Farm", sample.sort = "Group", x.label = "Farm", group_by = "Conventional.Organic") +
   # change y axis to be percentages instead of numbers
-  scale_y_continuous(labels = scales::percent) +
-  scale_fill_viridis(option = "mako", discrete = TRUE) + 
-  theme(text = element_text(size = 20)) + 
-  theme(legend.position = "none") +
-  labs(x = " ", fill=' ') +
-  scale_x_discrete(guide = guide_axis(angle = 90)) +
-  ggtitle("B")
+#  scale_y_continuous(labels = scales::percent) +
+#  scale_fill_viridis(option = "mako", discrete = TRUE) + 
+#  theme(text = element_text(size = 20)) + 
+#  theme(legend.position = "none") +
+#  labs(x = " ", fill=' ') +
+#  scale_x_discrete(guide = guide_axis(angle = 90)) +
+#  ggtitle("B")
 
-C <- calves %>% plot_composition(average_by = "Farm", sample.sort = "Conventional.Organic", x.label = "Farm", group_by = "Conventional.Organic") +
+#legend <- cows %>% plot_composition(average_by = "Farm", sample.sort = "Conventional.Organic", x.label = "Farm", group_by = "Conventional.Organic") +
   # change y axis to be percentages instead of numbers
-  scale_y_continuous(labels = scales::percent) +
-  scale_fill_viridis(option = "mako", discrete = TRUE) + 
-  theme(text = element_text(size = 20)) + 
-  theme(legend.position = "none") +
-  labs(x = " ", fill=' ') +
-  scale_x_discrete(guide = guide_axis(angle = 90)) +
-  ggtitle("C")
-
-D <- cows %>% plot_composition(average_by = "Farm", sample.sort = "Conventional.Organic", x.label = "Farm", group_by = "Conventional.Organic") +
-  # change y axis to be percentages instead of numbers
-  scale_y_continuous(labels = scales::percent) +
-  scale_fill_viridis(option = "mako", discrete = TRUE) + 
-  theme(text = element_text(size = 20)) + 
-  theme(legend.position = "none") +
-  labs(x = " ", fill=' ') +
-  scale_x_discrete(guide = guide_axis(angle = 90)) +
-  ggtitle("D")
-
-legend <- cows %>% plot_composition(average_by = "Farm", sample.sort = "Conventional.Organic", x.label = "Farm", group_by = "Conventional.Organic") +
-  # change y axis to be percentages instead of numbers
-  scale_y_continuous(labels = scales::percent) +
-  scale_fill_viridis(option = "mako", discrete = TRUE) + 
-  theme(text = element_text(size = 40)) + 
-  theme(legend.position = "top") +
-  labs(x = " ", fill=' ') +
-  scale_x_discrete(guide = guide_axis(angle = 90)) +
-  ggtitle("D")
+#  scale_y_continuous(labels = scales::percent) +
+#  scale_fill_viridis(option = "mako", discrete = TRUE) + 
+#  theme(text = element_text(size = 40)) + 
+#  theme(legend.position = "top") +
+#  labs(x = " ", fill=' ') +
+#  scale_x_discrete(guide = guide_axis(angle = 90)) +
+#  ggtitle("D")
 
 #(A|B)/(C|D)
 
@@ -102,19 +105,19 @@ legend <- cows %>% plot_composition(average_by = "Farm", sample.sort = "Conventi
 # all
 
 # Create user-defined function, which extracts legends from ggplots
-extract_legend <- function(my_ggp) {
-  step1 <- ggplot_gtable(ggplot_build(my_ggp))
-  step2 <- which(sapply(step1$grobs, function(x) x$name) == "guide-box")
-  step3 <- step1$grobs[[step2]]
-  return(step3)
-}
+#extract_legend <- function(my_ggp) {
+#  step1 <- ggplot_gtable(ggplot_build(my_ggp))
+#  step2 <- which(sapply(step1$grobs, function(x) x$name) == "guide-box")
+#  step3 <- step1$grobs[[step2]]
+#  return(step3)
+#}
 
 # Apply user-defined function to extract legend
-shared_legend <- extract_legend(legend)
+# shared_legend <- extract_legend(legend)
 
 # Draw plots with shared legend
-all <- grid.arrange(arrangeGrob(A, B, C, D, ncol = 2, nrow = 2),
-                    shared_legend, nrow = 2, heights = c(10, 1))
+# all <- grid.arrange(arrangeGrob(A, B, C, D, ncol = 2, nrow = 2),
+#                    shared_legend, nrow = 2, heights = c(10, 1))
 
-ggsave(all, filename = "plots/paper/figure2.pdf", dpi = 600, width = 24, height = 20)
+ggsave(A, filename = "plots/paper/figure2.pdf", dpi = 600, width = 24, height = 20)
 
